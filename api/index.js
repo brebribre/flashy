4b7e7c04cf8ab3ff49a3a588273a91a3f88450ff
brebribre <70563428+brebribre@users.noreply.github.com>
2023-08-19 16:10:51 +0200

    authentication api-side setup

--- api/index.js
+++ api/index.js
@@ -1,10 +1,26 @@
+
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
+const session = require('express-session');
+const passport = require("passport");
+const passportLocalMongoose = require("passport-local-mongoose");
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const findOrCreate = require('mongoose-findorcreate');
+const path = require('path');
 
 const app = express();
 app.use(express.json());
+app.use(express.static("public"));
 app.use(cors()); // stop cross-origin errors
+app.use(session({
+  secret: "Our little secret.",
+  resave: false,
+  saveUninitialized: false
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
 
 mongoose.connect("mongodb+srv://alvinbryan78:BryanAlvin123@cluster0.lnlrf4e.mongodb.net/flashy?retryWrites=true&w=majority",{
     useNewUrlParser: true,
@@ -16,6 +32,46 @@ mongoose.connect("mongodb+srv://alvinbryan78:BryanAlvin123@cluster0.lnlrf4e.mong
 //initialize the models
 const Card = require('./models/Card');
 const Deck = require('./models/Deck');
+
+const userSchema = new mongoose.Schema ({
+  email: String,
+  password: String,
+  googleId: String,
+  secret: String
+});
+
+userSchema.plugin(passportLocalMongoose);
+userSchema.plugin(findOrCreate);
+
+const User = new mongoose.model("User", userSchema);
+
+passport.use(User.createStrategy());
+
+passport.serializeUser(function(user, done) {
+  done(null, user.id);
+});
+
+passport.deserializeUser(function(id, done) {
+  User.findById(id, function(err, user) {
+    done(err, user);
+  });
+});
+
+//AUTHENTICATION OAUTH GOOGLE 2.0
+passport.use(new GoogleStrategy({
+  clientID: "445186148995-grerlb6ir809oennp4l502fuj5sijqbk.apps.googleusercontent.com",
+  clientSecret: process.env.CLIENT_SECRET,
+  callbackURL: "http://localhost:3000/auth/google/secrets",
+  userProfileURL: "https://www.googleapis.com/oauth2/v3/userinfo"
+},
+function(accessToken, refreshToken, profile, cb) {
+  console.log(profile);
+
+  User.findOrCreate({ googleId: profile.id }, function (err, user) {
+    return cb(err, user);
+  });
+}
+));
 
 //create the routes
 app.get('/decks', async(req,res)=>{
@@ -107,7 +163,6 @@ app.post('/deck/:deckId/card/:cardId', async (req, res) => {
     }
   });
 
-
 app.delete('/deck/:deckId/card/:cardId', async(req, res) => {
     try {
         const { deckId, cardId } = req.params;
@@ -152,6 +207,11 @@ app.delete('/card/delete/:id', async(req, res) => {
     res.json(result)
 })
 
-app.listen(3000, () => {
-    console.log('Server started on port 3000');
+// Serve the React app
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/public/index.html'));
+});
+
+app.listen(3001, () => {
+    console.log('Server started on port 3001');
 })
